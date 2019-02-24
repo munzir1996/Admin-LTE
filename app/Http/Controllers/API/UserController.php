23e0eaedfdc7d6e87f31ce203fa77dc5ab630fa4
munzir1996 <munzirmukhtar996@gmail.com>
2019-02-24 13:37:50 +0200

    V 1.0

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return User::latest()->paginate(10);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'name' => 'required|string|max:191',
+            'email' => 'required|string|email|max:191|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        return User::create([
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'type' => $request['type'],
+            'bio' => $request['bio'],
+            'password' => Hash::make($request['password']),
+            'photo' => $request['photo'],
+        ]);
+
+
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+
+        //Delete the user
+        $user->delete();
+    }
+}
---
+++ resources/js/components/Users.vue
@@ -0,0 +1,189 @@
+<template>
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Users Table</h3>
+
+                        <div class="card-tools">
+
+                            <button class="btn btn-success" data-toggle="modal" data-target="#addNew">Add New
+                                <i class="fas fa-user-plus fa-fw"></i>
+                            </button>
+
+                        </div>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body table-responsive p-0">
+                        <table class="table table-hover">
+                            <tbody>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Type</th>
+                                    <th>Registered At</th>
+                                    <th>Modify</th>
+                                </tr>
+                                <tr v-for="user in users" :key="user.id">
+                                    <td>{{user.id}}</td>
+                                    <td>{{user.name}}</td>
+                                    <td>{{user.email}}</td>
+                                    <td>{{user.type | upText}}</td>
+                                    <td>{{user.created_at | myDate}}</td>
+                                    <td>
+                                        <a href="">
+                                            <i class="fa fa-edit blue"></i>
+                                        </a>
+                                        /
+                                        <a href="#" @click="deleteUser(user.id)">
+                                            <i class="fa fa-trash red"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+            </div>
+        </div>
+
+        <!-- Modal -->
+        <div class="modal fade" id="addNew" tabindex="-1" role="dialog" aria-labelledby="addNewLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="addNewLabel">Add New</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <form @submit.prevent="createUser">
+                        <div class="modal-body">
+
+                            <div class="form-group">
+                                <input v-model="form.name" type="text" name="name" class="form-control" placeholder="Enter Your Name"
+                                    :class="{ 'is-invalid': form.errors.has('name') }">
+                                <has-error :form="form" field="name"></has-error>
+                            </div>
+
+                            <div class="form-group">
+                                <input v-model="form.email" type="email" name="email" class="form-control" placeholder="Email Address"
+                                    :class="{ 'is-invalid': form.errors.has('email') }">
+                                <has-error :form="form" field="email"></has-error>
+                            </div>
+
+                            <div class="form-group">
+                                <textarea v-model="form.bio" id="bio" name="bio" class="form-control" placeholder="Short Bio For User (Optional)"
+                                    :class="{ 'is-invalid': form.errors.has('bio') }"></textarea>
+                                <has-error :form="form" field="bio"></has-error>
+                            </div>
+
+                            <div class="form-group">
+                                <select v-model="form.type" name="type" id="type" class="form-control" placeholder="Short type For User (Optional)"
+                                    :class="{ 'is-invalid': form.errors.has('type') }">
+                                    <option value="">Select User Role</option>
+                                    <option value="admin">Admin</option>
+                                    <option value="author">Author</option>
+                                    <option value="user">Standard User</option>
+                                </select>
+                                <has-error :form="form" field="type"></has-error>
+                            </div>
+
+                            <div class="form-group">
+                                <input v-model="form.password" type="password" id="password" name="password" class="form-control"
+                                    :class="{ 'is-invalid': form.errors.has('password') }">
+                                <has-error :form="form" field="password"></has-error>
+                            </div>
+
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Create</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                users: {},
+                form: new Form({
+                    name: '',
+                    email: '',
+                    password: '',
+                    type: '',
+                    bio: '',
+                    photo: '',
+                })
+            }
+        },
+        methods: {
+            deleteUser(id) {
+                swal.fire({
+                    title: 'Are you sure?',
+                    text: "You won't be able to revert this!",
+                    type: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes, delete it!'
+                }).then((result) => {
+
+                    //Send Request To The Server
+                    if (result.value) {
+                        this.form.delete('api/user/' + id).then(() => {
+                            swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            )
+                            Fire.$emit('AfterCreate');
+                        }).catch(() => {
+                            swal('Failed!', 'There was something wrong', 'warning')
+                        })
+                    }
+                })
+            },
+            loadUsers() {
+                axios.get("api/user").then(({
+                    data
+                }) => (this.users = data.data));
+            },
+            createUser() {
+                this.$Progress.start();
+                this.form.post('api/user')
+                    .then(() => {
+                        Fire.$emit('AfterCreate');
+                        $('#addNew').modal('hide');
+                        Toast.fire({
+                            type: 'success',
+                            title: 'User Created successfully'
+                        })
+                        this.$Progress.finish();
+                    })
+                    .catch(() => {
+
+                    })
+
+            }
+        },
+        created() {
+            this.loadUsers();
+            Fire.$on('AfterCreate', () => {
+                this.loadUsers();
+            });
+            //setInterval(() => this.loadUsers(), 3000);
+        }
+    }
+
+</script>
